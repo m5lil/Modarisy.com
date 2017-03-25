498368b5fd4dfa0516a53c76c8cf0d67993fc0ea
Mahmoud Khalil <mkhleel01@gmail.com>
2017-03-25 19:59:19 +0200

    Finishing the basics

---
+++ app/Mail/Order.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Order extends Mailable
+{
+    use Queueable, SerializesModels;
+
+
+
+    protected $order;
+
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.orders')
+//            ->with([
+//                'orderName' => $this->order->name,
+//                'orderPrice' => $this->order->price,
+//            ])
+//            ->attach('/path/to/file', [
+//                'as' => 'name.pdf',
+//                'mime' => 'application/pdf',
+//            ]);
+
+
+//        $order = Order::findOrFail($orderId);
+
+        // Ship order...
+
+//        Mail::to($request->user())->send(new OrderShipped($order));
+
+
+
+
+        ;
+    }
+}
